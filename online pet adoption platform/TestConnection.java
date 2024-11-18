f069a8fced28216299a232d50c22484365dccfde
naman77789808 <namanswami789@gmail.com>
2024-11-18 18:13:05 +0530

    Create  TestConnection.java

---
+++ online pet adoption platform/TestConnection.java
@@ -0,0 +1,19 @@
+// TestConnection.java
+
+import java.sql.*;
+
+public class TestConnection {
+
+    public static void testDBConnection() {
+        try (Connection conn = DBConnection.getConnection()) {
+            if (conn != null) {
+                System.out.println("Database connection established successfully!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error occurred while connecting to the database.");
+            e.printStackTrace();
+        }
+    }
+}
